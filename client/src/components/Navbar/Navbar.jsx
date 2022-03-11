0d8be4d806900c29741b2059af67da25d988c5d7
Nekane Ibarlucea <nknib90@gmail.com>
2022-03-11 10:39:23 +0100

    api

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,1 +1,14 @@
 
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+  return (
+    <nav>
+     <div>This is Navbar</div>
+
+    </nav>
+  );
+}
+
+export default Navbar;
--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ require("./config")(app);
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes/index.routes");
-app.use("/api", allRoutes);
+app.use("/", allRoutes);
 
 const authRouter = require("./routes/auth.routes");
 app.use("/api/auth", authRouter);
