0f65af13d56f2fb2a3929f84feb87d45bd13f396
Nekane Ibarlucea <nknib90@gmail.com>
2022-03-15 17:47:32 +0100

    cards

--- client/src/pages/EventsMainPage.js
+++ client/src/pages/EventsMainPage.js
@@ -7,25 +7,25 @@ export default function EventsMainPage(){
     return(
         <div className="containerDiv">
             <div className="filter-btn">
-            <button type="button" class="btn btn-outline-info btn-rounded" data-mdb-ripple-color="dark">Choose a date</button>
+            <button type="button" className="btn btn-outline-info btn-rounded" data-mdb-ripple-color="dark">Choose a date</button>
             </div>
 
-            <div className="card bg-white text-white">
-            <div className="card-body">
-                <img className="card-img" src="/images/allEventsç.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                <div className="card-img-overlay">
-                    <Link to={`/events/list/all`} className="btn btn-outline-info btn-rounded">
-                    <h3>All events</h3>
-                    </Link>
+            <div className="card bg-white">
+                <div className="card-body">
+                    <img className="card-img" src="/images/allEventsç.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/all`} className="btn btn-outline-primary btn-rounded align-self-center mx-auto">
+                        <h3>All events</h3>
+                        </Link>
+                    </div>
                 </div>
             </div>
-            </div>
 
-            <div className="card bg-white text-white" >
+            <div className="card bg-white " >
                 <div className="card-body">
-                    <img src="/images/audience-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                    <div className="card-title">
-                        <Link to={`/events/list/concerts`} className="btn btn-primary">  
+                    <img className="card-img" src="/images/audience-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/concerts`} className="btn btn-outline-dark btn-rounded align-self-center mx-auto">  
                         <h3>Concerts</h3>
                         </Link>
                     </div>
@@ -33,10 +33,10 @@ export default function EventsMainPage(){
             </div>
 
                 <div className="card">
-                    <div className="card-body">
-                    <img src="/images/sportsPB/soccer-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                    <div className="card-title">
-                        <Link to={`/events/list/sport`} className="btn btn-primary">
+                 <div className="card-body">
+                    <img className="card-img" src="/images/sportsPB/soccer-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/sport`} className="btn btn-outline-dark btn-rounded align-self-center mx-auto">
                         <h3>Sport plans</h3>
                         </Link>
                     </div>
@@ -45,9 +45,9 @@ export default function EventsMainPage(){
 
                 <div className="card">
                     <div className="card-body">
-                    <img src="/images/spaghetti-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                    <div className="card-title">
-                        <Link to={`/events/list/cooking`} className="btn btn-primary">
+                    <img className="card-img" src="/images/spaghetti-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/cooking`} className="btn btn-outline-dark btn-rounded align-self-center mx-auto">
                         <h3>Cooking plans</h3>
                         </Link>
                     </div> 
@@ -56,9 +56,9 @@ export default function EventsMainPage(){
 
                 <div className="card">
                     <div className="card-body">
-                    <img src="/images/culturalPlans.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                    <div className="card-title">
-                        <Link to={`/events/list/cultural`} className="btn btn-primary">
+                    <img className="card-img" src="/images/culturalPlans.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/cultural`} className="btn btn-outline-dark btn-rounded align-self-center mx-auto">
                         <h3>Cultural plans</h3>
                         </Link>
                     </div> 
@@ -67,9 +67,9 @@ export default function EventsMainPage(){
 
                 <div className="card">
                     <div className="card-body">
-                    <img src="/images/pedralbes-festival.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                    <div className="card-title">
-                        <Link to={`/events/list/social`} className="btn btn-primary">
+                    <img className="card-img" src="/images/pedralbes-festival.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/social`} className="btn btn-outline-dark btn-rounded align-self-center mx-auto">
                         <h3>Social plans</h3>
                         </Link>
                     </div> 
@@ -78,9 +78,9 @@ export default function EventsMainPage(){
                 
                 <div className="card">
                     <div className="card-body">
-                    <img src="/images/sun-concert-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
-                    <div className="card-title">
-                        <Link to={`/events/list/other`} className="btn btn-primary">
+                    <img className="card-img" src="/images/sun-concert-1.jpg" alt="" style={{ maxWidth: "400px" }}/>
+                    <div className="card-img-overlay d-flex">
+                        <Link to={`/events/list/other`} className="btn btn-outline-dark btn-rounded align-self-center mx-auto">
                         <h3>Other plans</h3>
                         </Link>
                     </div> 
