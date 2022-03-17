88501092f8ccefe039004f7e1b59851f98efe3b6
Fer Sánchez <sangarfer@gmail.com>
2022-03-17 11:41:39 +0100

    spaces-cards css done

--- client/src/components/SpaceCard.jsx
+++ client/src/components/SpaceCard.jsx
@@ -5,9 +5,9 @@ export default function SpaceCard(props){
     const {_id, name, imageUrl, availableDates, availableHours, capacity, address } = props.space;
 
     return(
-        <div>
-         <div key={_id} className="spaceCard">
-                <img width={300} src={imageUrl} alt="spaceImg" />
+        <div className="spaceCardContainer">
+            <div key={_id} className="spaceCard">
+                <img src={imageUrl} alt="spaceImg" />
                 <h3>{name}</h3>
                 <h3>{address.direction}</h3>
                 <h2>{availableDates}</h2>
